624d80ca8110a4195d13250c09a9fd64f607c498
Brian <briankariuki9975@gmail.com>
2026-04-05 18:09:59 +0300

    Added a sidebar with both the customers and bookings links

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -2,13 +2,14 @@ import type React from "react";
 import { useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { LogOut, CalendarCheck, Contact } from "lucide-react";
 
 
 
 
 
 export default function ProtectedRoute({children}: {children: React.ReactNode}) {
-const {user, loading} = useAuth()
+const {user, loading, logout} = useAuth()
 const navigate = useNavigate()
 
 
@@ -29,10 +30,40 @@ if (!user) {
  return null
 }
   return (
-    <div>
+    <div className="overflow-hidden bg-gradient-to-b from-blue-500 from-10% to-lime-300 to-90% min-h-screen">
+  {/* Topbar */}
+  <div className="bg-blue-200 border-b-2 border-lime-800 shadow-xl items-center justify-between flex py-3 px-2">
+    <div> <h1 className="font-extrabold text-lg">C & B System</h1></div>
+   <div>
+    <div className="bg-lime-300 flex justify-center items-center rounded-full h-9 w-9">
+        <div><p className="font-bold">J</p></div>
+        
+    </div>
+   </div>
+  </div>
+    
 
-    <h1>Hello World</h1>
-    <main>{children}</main>
+    <div className="sm:flex">
+    {/* sidebar */}
+    <div className="hidden flex-start sm:block min-h-screen top-0 sticky bg-blue-200 w-60">
+        <div className=" flex flex-col items-center p-2">
+
+            <div className="my-3 ">
+
+                <button onClick={() => navigate("/customers")} className="bg-blue-900  my-2 py-2 px-15 cursor-pointer hover:bg-blue-800 text-blue-100 rounded-md" ><div className="flex items-center gap-2 justify-center"><Contact/><div>Customers</div></div></button>
+            
+            </div>
+            <div>
+     
+                <button onClick={() => navigate("/bookings")} className="bg-blue-900  my-2  px-15 p-2 cursor-pointer hover:bg-blue-800 text-blue-100 rounded-md" ><div className="flex justify-center gap-2 items-center"><CalendarCheck/><div>Bookings</div></div></button>
+     
+            </div>
+        <button onClick={logout} className="bg-lime-300 mt-5 shadow-lg hover:opacity-80 cursor-pointer w-40 p-2 rounded-md"><div className="flex gap-2 justify-center items-center "><LogOut size={20}/><div>Log Out</div></div></button>
+        </div>
+    </div>
+
+    <main className="p-2 overflow-y-auto flex-1">{children}</main>
+    </div>
     </div>
   )
 }
